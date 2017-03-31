e3d60f742df8793ffab48e70c3729c76a07fda23
Jia-Wei <User@MacBook-Pro.local>
2017-04-01 04:09:31 +0800

    Clock (tkinter, time, thread)

---
+++ clock.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+"""
+This is my practice.
+I need change label text.
+Cause mainloop() , I must use thread to finish it.
+"""
+
+from Tkinter import *
+import time
+import thread
+
+def print_time (delay):
+	while True:
+		time.sleep(1)
+		label.configure(text=time.strftime("%Y-%m-%d %H:%M:%S" , time.localtime()))
+
+root=Tk()
+root.title("Data & Time")
+root.minsize(300,25)
+label=Label(root,text=time.strftime("%Y-%m-%d %H:%M:%S" , time.localtime()))
+label.pack()
+try:
+	thread.start_new_thread( print_time , (1,))
+except:
+	label.configure(text="Error: unable to start thread")
+
+root.mainloop()
+
+
+
